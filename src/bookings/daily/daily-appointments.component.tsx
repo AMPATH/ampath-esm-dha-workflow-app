9b8d88f6c9b894faf06d71d827d8f12e065a1e8b
maikofelix47 <maikofelix89@gmail.com>
2025-12-16 15:17:29 +0300

    (feat) Add Daily Bookings feature

--- src/bookings/daily/daily-appointments.component.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-interface DailyAppointmentsProps {
-  startDate: string;
-  locationUuid: string;
-}
-const DailyAppointments: React.FC<DailyAppointmentsProps> = ({ startDate, locationUuid }) => {
-  return <>Daily Appoinments</>;
-};
-export default DailyAppointments;
