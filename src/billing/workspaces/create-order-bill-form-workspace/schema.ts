67115deb4eb6f2668c8b8254ac210b05eca7b1f7
Walter Kiprono <walterkiprono81@gmail.com>
2026-04-05 22:23:53 +0300

    chore: set default payment mode for orders

--- src/billing/workspaces/create-order-bill-form-workspace/schema.ts
+++ src/billing/workspaces/create-order-bill-form-workspace/schema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 const validationSchema = z.object({
-    unitPrice: z.string({ required_error: "Unit price is required" }).min(1),
+    unitPrice: z.string({ required_error: "Unit price is required" }),
     quantity: z.number({ required_error: "Quantity is required" }),
     billableItem: z.string().optional()
 });
