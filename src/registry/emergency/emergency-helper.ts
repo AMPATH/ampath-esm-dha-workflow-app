d8c2be3e78dfce7ff63e2a01aa5c32efecee4851
Denzel Kipkemoi <denzelkipkemoi@Denzels-MacBook-Pro.local>
2026-09-12 12:02:21 +0300

    Fix: Updated visit attributes when creating emeregncy claim visit

--- src/registry/emergency/emergency-helper.ts
+++ src/registry/emergency/emergency-helper.ts
@@ -1,8 +1,14 @@
 import { createVisit } from '../../resources/visit.resource';
-import { type CreateVisitDto } from '../types';
+import { type CreateVisitDto, type VisitAttribute } from '../types';
 import { VisitTypeUuids } from '../../shared/constants/visit-types';
 
-export const createAmrsVisit = async (locationUuid: string, patientUuid: string) => {
+type EmergencyClaim = {
+  authorization_code: string;
+  scheme_code: string;
+  service_type: string;
+};
+
+export const createAmrsVisit = async (locationUuid: string, patientUuid: string, claim: EmergencyClaim) => {
   if (!locationUuid || !patientUuid) {
     throw new Error('Missing location or patient for AMRS visit');
   }
@@ -14,6 +20,23 @@ export const createAmrsVisit = async (locationUuid: string, patientUuid: string)
     stopDatetime: null,
     patient: patientUuid,
   };
+
+  const attributes: VisitAttribute[] = [
+    {
+      attributeType: '4962a633-c4f8-474c-857c-5c68c72fbbe3',
+      value: claim.authorization_code,
+    },
+    {
+      attributeType: '79072572-80c0-4a38-9da0-afe207e3ef2d',
+      value: claim.scheme_code,
+    },
+    {
+      attributeType: '97d892fe-38a4-4cfb-bdf7-2a03dff6e7cf',
+      value: claim.service_type,
+    },
+  ];
+
+  visitDto.attributes = attributes;
 
   const visit = await createVisit(visitDto);
   if (!visit?.uuid) {
